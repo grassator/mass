9b8475b3aa5f42aa9cefc5a12ccf01e21f8e8dad
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2020-05-24 16:09:41 +0200

    Introduce more macros for Function builder

--- spec.c
+++ spec.c
@@ -8,9 +8,21 @@
 #include "encoding.c"
 
 typedef struct {
+  s32 *location;
+  u64 ip;
+} Patch_32;
+
+typedef struct Function_Return_Patch {
+  Patch_32 patch;
+  struct Function_Return_Patch *next;
+} Function_Return_Patch;
+
+typedef struct {
   s32 stack_reserve;
   u8 next_argument_index;
   Buffer buffer;
+
+  Function_Return_Patch *return_patch_list;
 
   Descriptor_Function descriptor;
 } Function_Builder;
@@ -171,11 +183,47 @@ divide(
   return temp;
 }
 
+Patch_32
+make_jz(
+  Function_Builder *fn
+) {
+  encode(&fn->buffer, (Instruction) {jz, {imm32(0xcc), 0, 0}});
+  u64 ip = fn->buffer.occupied;
+  s32 *location = (s32 *)(fn->buffer.memory + fn->buffer.occupied - sizeof(s32));
+  return (const Patch_32) { .location = location, .ip = ip };
+}
+
+Patch_32
+make_jmp(
+  Function_Builder *fn
+) {
+  encode(&fn->buffer, (Instruction) {jmp, {imm32(0xcc), 0, 0}});
+  u64 ip = fn->buffer.occupied;
+  s32 *location = (s32 *)(fn->buffer.memory + fn->buffer.occupied - sizeof(s32));
+  return (const Patch_32) { .location = location, .ip = ip };
+}
+
+void
+patch_jump_to_here(
+  Function_Builder *fn,
+  Patch_32 patch
+) {
+  *patch.location = (s32) (fn->buffer.occupied - patch.ip);
+}
+
+void
+patch_jump_to_ip(
+  Patch_32 patch,
+  u64 ip
+) {
+  *patch.location = (s32) (ip - patch.ip);
+}
 
 Function_Builder
 fn_begin() {
   Function_Builder fn = {
-    .stack_reserve = 0x0,
+    .stack_reserve = 0,
+    .return_patch_list = 0,
     .buffer = make_buffer(1024, PAGE_EXECUTE_READWRITE),
     .descriptor = (const Descriptor_Function) {0}
   };
@@ -203,6 +251,12 @@ fn_end(
     // @Volatile @ReserveStack
     encode(&builder->buffer, (Instruction) {sub, {rsp, imm32(stack_size), 0}});
     builder->buffer.occupied = save_occupied;
+  }
+
+  Function_Return_Patch *return_patch = builder->return_patch_list;
+  while (return_patch) {
+    patch_jump_to_here(builder, return_patch->patch);
+    return_patch = return_patch->next;
   }
 
   encode(&builder->buffer, (Instruction) {add, {rsp, imm32(stack_size), 0}});
@@ -280,11 +334,11 @@ fn_arg(
 
 Value *
 fn_return(
-  Function_Builder *fn,
+  Function_Builder *builder,
   Value *to_return
 ) {
   // FIXME check that all return paths return the same type
-  *fn->descriptor.returns = *to_return;
+  *builder->descriptor.returns = *to_return;
 
   u32 byte_size = descriptor_byte_size(&to_return->descriptor);
   assert(byte_size <= 8);
@@ -296,48 +350,19 @@ fn_return(
   };
 
   if (to_return->descriptor.type != Descriptor_Type_Void) {
-    encode(&fn->buffer, (Instruction) {mov, {return_operand, to_return->operand, 0}});
+    encode(&builder->buffer, (Instruction) {mov, {return_operand, to_return->operand, 0}});
   }
+  Function_Return_Patch *return_patch = temp_allocate(Function_Return_Patch);
+  *return_patch = (const Function_Return_Patch) {
+    .patch = make_jmp(builder),
+    .next = builder->return_patch_list,
+  };
+  builder->return_patch_list = return_patch;
+
   return to_return;
 }
 
-Value *
-make_constant_s32(
-  s32 integer
-) {
-  Function_Builder builder = fn_begin();
-  {
-    fn_return(&builder, value_from_s32(integer));
-  }
-  return fn_end(&builder);
-}
-
-Value *
-make_identity_s64() {
-  Function_Builder builder = fn_begin();
-  {
-    Value *arg0 = fn_arg(&builder, &descriptor_s64);
-    fn_return(&builder, arg0);
-  }
-  return fn_end(&builder);
-}
-
-Value *
-make_increment_s64() {
-  Function_Builder builder = fn_begin();
-  {
-    Value *x = reserve_stack(&builder, &descriptor_s64);
-    assign(&builder, x->operand, imm32(1));
-    Value *y = reserve_stack(&builder, &descriptor_s64);
-    assign(&builder, y->operand, imm32(2));
-    Value *arg0 = fn_arg(&builder, &descriptor_s64);
-    Value *temp = minus(&builder, arg0, x);
-    Value *temp2 = plus(&builder, temp, y);
-    fn_return(&builder, temp2);
-  }
-  return fn_end(&builder);
-}
-
+// TODO create variadic macro for call_function_value
 Value *
 call_function_value(
   Function_Builder *builder,
@@ -364,82 +389,30 @@ call_function_value(
   return descriptor->returns;
 }
 
-Value *
-make_call_no_arg_return_s32() {
-  Function_Builder builder = fn_begin();
-  {
-    Value *returns = malloc(sizeof(Value));
-    *returns = (const Value){0};
-    returns->descriptor = descriptor_s32;
-    returns->operand = rax;
+#define Function(_id_) \
+  Value *_id_ = 0; \
+  for (Function_Builder builder_ = fn_begin(); !(_id_) ; _id_ = fn_end(&builder_))
 
-    Descriptor arg0_descriptor = {
-      .type = Descriptor_Type_Function,
-      .function = {
-        .argument_list = 0,
-        .argument_count = 0,
-        .returns = returns,
-      }
-    };
-    Value *arg0 = fn_arg(&builder, &arg0_descriptor);
+#define Return(_value_) \
+  fn_return(&builder_, _value_)
 
-    fn_return(&builder, call_function_value(&builder, arg0, 0, 0));
-  }
-  return fn_end(&builder);
-}
+#define Arg(_id_, _descriptor_) \
+  Value *_id_ = fn_arg(&builder_, (_descriptor_))
 
-Value *
-make_partial_application_s64(
-  Value *original_fn,
-  s64 arg
-) {
-  Function_Builder builder = fn_begin();
-  {
-    fn_return(&builder, call_function_value(&builder, original_fn, value_from_s64(arg), 1));
-  }
-  return fn_end(&builder);
-}
+#define Arg_s32(_id_) Arg((_id_), &descriptor_s32)
+#define Arg_s64(_id_) Arg((_id_), &descriptor_s64)
 
-typedef struct {
-  s32 *location;
-  u64 ip;
-} Patch_32;
+#define Stack(_id_, _descriptor_, _value_) \
+  Value *_id_ = reserve_stack(&builder_, (_descriptor_)); \
+  assign(&builder_, (_id_)->operand, (_value_)->operand)
 
-Patch_32
-make_jz(
-  Function_Builder *fn
-) {
-  encode(&fn->buffer, (Instruction) {jz, {imm32(0xcc), 0, 0}});
-  u64 ip = fn->buffer.occupied;
-  s32 *location = (s32 *)(fn->buffer.memory + fn->buffer.occupied - sizeof(s32));
-  return (const Patch_32) { .location = location, .ip = ip };
-}
+#define Stack_s32(_id_, _value_) Stack((_id_), &descriptor_s32, _value_)
+#define Stack_s64(_id_, _value_) Stack((_id_), &descriptor_s64, _value_)
 
-Patch_32
-make_jmp(
-  Function_Builder *fn
-) {
-  encode(&fn->buffer, (Instruction) {jmp, {imm32(0xcc), 0, 0}});
-  u64 ip = fn->buffer.occupied;
-  s32 *location = (s32 *)(fn->buffer.memory + fn->buffer.occupied - sizeof(s32));
-  return (const Patch_32) { .location = location, .ip = ip };
-}
-
-void
-patch_jump_to_here(
-  Function_Builder *fn,
-  Patch_32 patch
-) {
-  *patch.location = (s32) (fn->buffer.occupied - patch.ip);
-}
-
-void
-patch_jump_to_ip(
-  Patch_32 patch,
-  u64 ip
-) {
-  *patch.location = (s32) (ip - patch.ip);
-}
+#define Plus(_a_, _b_) plus(&builder_, _a_, _b_)
+#define Minus(_a_, _b_) minus(&builder_, _a_, _b_)
+#define Multiply(_a_, _b_) multiply(&builder_, _a_, _b_)
+#define Divide(_a_, _b_) divide(&builder_, _a_, _b_)
 
 Patch_32 make_if(
   Function_Builder *builder,
@@ -451,7 +424,7 @@ Patch_32 make_if(
 }
 
 #define If(_value_) \
-  for (Patch_32 patch__ = make_if(&builder, _value_), *dummy__ = 0; !(dummy__++) ; patch_jump_to_here(&builder, patch__))
+  for (Patch_32 patch__ = make_if(&builder_, _value_), *dummy__ = 0; !(dummy__++) ; patch_jump_to_here(&builder_, patch__))
 
 typedef enum {
   Compare_Equal,
@@ -490,26 +463,8 @@ compare(
   return result;
 }
 
-#define Eq(_a_, _b_) compare(&builder, Compare_Equal, (_a_), (_b_))
-#define Less(_a_, _b_) compare(&builder, Compare_Less, (_a_), (_b_))
-
-Value *
-make_is_non_zero() {
-  Function_Builder builder = fn_begin();
-  {
-    Patch_32 return_patch = {0};
-    Value *arg0 = fn_arg(&builder, &descriptor_s32);
-
-    If(Eq(arg0, value_from_s32(0))) {
-      fn_return(&builder, value_from_s32(0));
-      return_patch = make_jmp(&builder);
-    }
-
-    fn_return(&builder, value_from_s32(1));
-    patch_jump_to_here(&builder, return_patch);
-  }
-  return fn_end(&builder);
-}
+#define Eq(_a_, _b_) compare(&builder_, Compare_Equal, (_a_), (_b_))
+#define Less(_a_, _b_) compare(&builder_, Compare_Less, (_a_), (_b_))
 
 spec("mass") {
   before() {
@@ -521,26 +476,44 @@ spec("mass") {
   }
 
   it("should create function that will return 42") {
-    Value *the_answer = make_constant_s32(42);
+    Function(the_answer) {
+      Return(value_from_s32(42));
+    }
     s32 result = value_as_function(the_answer, fn_type_void_to_s32)();
     check(result == 42);
   }
 
   it("should create function that returns s64 value that was passed") {
-    Value *id_value = make_identity_s64();
-    s64 result = value_as_function(id_value, fn_type_s64_to_s64)(42);
+    Function(id) {
+      Arg_s64(x);
+      Return(x);
+    }
+    s64 result = value_as_function(id, fn_type_s64_to_s64)(42);
     check(result == 42);
   }
 
   it("should create function increments s64 value passed to it") {
-    Value *inc_s64 = make_increment_s64();
-    s64 result = value_as_function(inc_s64, fn_type_s64_to_s64)(42);
+    Function(increment) {
+      // TODO add a check that all argument are defined before stack variables
+      Arg_s64(x);
+
+      Stack_s64(one, value_from_s32(1));
+      Stack_s64(two, value_from_s32(2));
+
+      Return(Plus(x, Minus(two, one)));
+    }
+    s64 result = value_as_function(increment, fn_type_s64_to_s64)(42);
     check(result == 43);
   }
 
   it("should create a function to call a no argument fn") {
-    Value *the_answer = make_constant_s32(42);
-    Value *caller = make_call_no_arg_return_s32();
+    Function(the_answer) {
+      Return(value_from_s32(42));
+    }
+    Function(caller) {
+      Arg(fn, &the_answer->descriptor);
+      Return(call_function_value(&builder_, fn, 0, 0));
+    }
     s32 result = value_as_function(caller, fn_type__void_to_s32__to_s32)(
       value_as_function(the_answer, fn_type_void_to_s32)
     );
@@ -548,15 +521,26 @@ spec("mass") {
   }
 
   it("should create a partially applied function") {
-    Value *id_value = make_identity_s64();
-    Value *partial_fn_value = make_partial_application_s64(id_value, 42);
-    fn_type_void_to_s64 the_answer = value_as_function(partial_fn_value, fn_type_void_to_s64);
+    Function(id) {
+      Arg_s64(x);
+      Return(x);
+    }
+    Function(partial) {
+      Return(call_function_value(&builder_, id, value_from_s64(42), 1));
+    }
+    fn_type_void_to_s64 the_answer = value_as_function(partial, fn_type_void_to_s64);
     s64 result = the_answer();
     check(result == 42);
   }
 
   it("should have a function that returns 0 if arg is zero, 1 otherwise") {
-    Value *is_non_zero_value = make_is_non_zero();
+    Function(is_non_zero_value) {
+      Arg_s32(x);
+      If(Eq(x, value_from_s32(0))) {
+        Return(value_from_s32(0));
+      }
+      Return(value_from_s32(1));
+    }
     fn_type_s32_to_s32 is_non_zero = value_as_function(is_non_zero_value, fn_type_s32_to_s32);
     s32 result = is_non_zero(0);
     check(result == 0);
@@ -565,47 +549,38 @@ spec("mass") {
   }
 
   it("should return 3rd argument") {
-    Function_Builder builder = fn_begin();
-    {
-      (void)fn_arg(&builder, &descriptor_s64);
-      (void)fn_arg(&builder, &descriptor_s64);
-      Value *arg2 = fn_arg(&builder, &descriptor_s64);
-      fn_return(&builder, arg2);
-    }
-    Value *value = fn_end(&builder);
-    fn_type_s64_s64_s64_to_s64 third = value_as_function(value, fn_type_s64_s64_s64_to_s64);
+    Function(third) {
+      Arg_s64(arg0);
+      Arg_s64(arg1);
+      Arg_s64(arg2);
 
-    s64 result = third(1, 2, 3);
+      (void)arg0; // unused
+      (void)arg1; // unused
+
+      Return(arg2);
+    }
+    s64 result = value_as_function(third, fn_type_s64_s64_s64_to_s64)(1, 2, 3);
     check(result == 3);
   }
 
   it("should make function that multiplies by 2") {
-    Function_Builder builder = fn_begin();
-    {
-      Value *arg0 = fn_arg(&builder, &descriptor_s64);
-      Value *two = value_from_s32(2);
-      Value *to_return = multiply(&builder, arg0, two);
-      fn_return(&builder, to_return);
+    Function(twice) {
+      Arg_s64(x);
+      Return(Multiply(x, value_from_s32(2)));
     }
-    Value *value = fn_end(&builder);
-    fn_type_s64_to_s64 twice = value_as_function(value, fn_type_s64_to_s64);
 
-    s64 result = twice(42);
+    s64 result = value_as_function(twice, fn_type_s64_to_s64)(42);
     check(result == 84);
   }
 
   it("should make function that divides two number") {
-    Function_Builder builder = fn_begin();
-    {
-      Value *arg0 = fn_arg(&builder, &descriptor_s32);
-      Value *arg1 = fn_arg(&builder, &descriptor_s32);
-      Value *to_return = divide(&builder, arg0, arg1);
-      fn_return(&builder, to_return);
+    Function(half) {
+      Arg_s32(x);
+      Arg_s32(y);
+      Return(Divide(x, y));
     }
-    Value *value = fn_end(&builder);
-    fn_type_s32_s32_to_s32 divide_fn = value_as_function(value, fn_type_s32_s32_to_s32);
 
-    s32 result = divide_fn(-42, 2);
+    s32 result = value_as_function(half, fn_type_s32_s32_to_s32)(-42, 2);
     check(result == -21);
   }
 
@@ -636,14 +611,11 @@ spec("mass") {
 
     Value puts_value = c_function_value("int puts(const char*)", (fn_type_opaque) puts);
 
-    Function_Builder builder = fn_begin();
-    {
-      call_function_value(&builder, &puts_value, &message_value, 1);
-      fn_return(&builder, &void_value);
+    Function(hello) {
+      call_function_value(&builder_, &puts_value, &message_value, 1);
     }
-    Value *value = fn_end(&builder);
 
-    value_as_function(value, fn_type_void_to_void)();
+    value_as_function(hello, fn_type_void_to_void)();
   }
 
 
@@ -675,12 +647,9 @@ spec("mass") {
       .pointer_to = &size_struct_descriptor,
     };
 
-    Function_Builder builder = fn_begin();
-    {
-      Value *arg0 = fn_arg(&builder, &size_struct_pointer_descriptor);
-
-      // TODO figure out how to deal with temporary values
-      encode(&builder.buffer, (Instruction) {mov, {rcx, arg0->operand, 0}});
+    Function(area) {
+      Arg(size_struct, &size_struct_pointer_descriptor);
+      encode(&builder_.buffer, (Instruction) {mov, {rcx, size_struct->operand, 0}});
 
       Value width_value = {
         .descriptor = *width_field->descriptor,
@@ -706,14 +675,11 @@ spec("mass") {
         }
       };
 
-      fn_return(&builder, multiply(&builder, &width_value, &height_value));
+      Return(Multiply(&width_value, &height_value));
     }
-    Value *value = fn_end(&builder);
-
-    fn_type_voidp_to_s32 area = value_as_function(value, fn_type_voidp_to_s32);
 
     struct { s32 width; s32 height; } size = { 10, 42 };
-    s32 result = area(&size);
+    s32 result = value_as_function(area, fn_type_voidp_to_s32)(&size);
     check(result == 420);
   }
 
@@ -733,19 +699,15 @@ spec("mass") {
       .pointer_to = &array_descriptor,
     };
 
-    Function_Builder builder = fn_begin();
-    {
-      Value *arg0 = fn_arg(&builder, &array_pointer_descriptor);
-      Value *index = reserve_stack(&builder, &descriptor_s32);
-      encode(&builder.buffer, (Instruction) {mov, {index->operand, imm32(0), 0}});
-
-      Value *temp = reserve_stack(&builder, &arg0->descriptor);
-      encode(&builder.buffer, (Instruction) {mov, {temp->operand, arg0->operand, 0}});
+    Function(increment) {
+      Arg(arr, &array_pointer_descriptor);
+      Stack_s32(index, value_from_s32(0));
+      Stack(temp, &arr->descriptor, arr);
 
       u32 item_byte_size = descriptor_byte_size(array_pointer_descriptor.pointer_to->array.item);
-      u64 loop_start_ip = builder.buffer.occupied;
+      u64 loop_start_ip = builder_.buffer.occupied;
       {
-        encode(&builder.buffer, (Instruction) {mov, {rax, temp->operand, 0}});
+        encode(&builder_.buffer, (Instruction) {mov, {rax, temp->operand, 0}});
 
         Operand pointer = {
           .type = Operand_Type_Memory_Indirect,
@@ -755,24 +717,20 @@ spec("mass") {
             .displacement = 0,
           }
         };
-        encode(&builder.buffer, (Instruction) {inc, {pointer, 0, 0}});
+        encode(&builder_.buffer, (Instruction) {inc, {pointer, 0, 0}});
 
-        // Loop body goes here
-        encode(&builder.buffer, (Instruction) {add, {temp->operand, imm32(item_byte_size), 0}});
+        encode(&builder_.buffer, (Instruction) {add, {temp->operand, imm32(item_byte_size), 0}});
       }
 
       // TODO this is a do {} while loop and we should use while
-      encode(&builder.buffer, (Instruction) {inc, {index->operand, 0, 0}});
+      encode(&builder_.buffer, (Instruction) {inc, {index->operand, 0, 0}});
       // TODO check that the descriptor in indeed an array
       s32 length = (s32)array_pointer_descriptor.pointer_to->array.length;
       If(Less(index, value_from_s32(length))) {
-        patch_jump_to_ip(make_jmp(&builder), loop_start_ip);
+        patch_jump_to_ip(make_jmp(&builder_), loop_start_ip);
       }
-      // for (int i = 0; i < 3; ++i)
-      fn_return(&builder, &void_value);
     }
-    Value *value = fn_end(&builder);
-    value_as_function(value, fn_type_s32p_to_void)(array);
+    value_as_function(increment, fn_type_s32p_to_void)(array);
 
     check(array[0] == 2);
     check(array[1] == 3);
