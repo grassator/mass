8e6cb52fc2e7acc81e2de387b5d737103c07f5e1
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2020-05-28 18:55:41 +0200

    Fix test.bat to not copy paste the build

--- test.bat
+++ test.bat
@@ -1,21 +1,4 @@
 @echo off
 
-if not defined DevEnvDir (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-)
-
-rmdir /S /Q build
-mkdir build
-
-pushd build
-cl /Fe:test /Od /std:c++latest /nologo /WX /FC /Zo /Zi^
-   /Wall /wd4711 /wd4068 /wd4710 /wd4204 /wd4200 /wd4221 /wd4057^
-   /wd4255 /wd4505 /wd4201 /wd4668 /wd4820 /wd5045 /wd4100^
-   /D UNICODE /D _UNICODE^
-   ..\spec.c
-if %errorlevel% neq 0 (
-  popd
-  exit /b 1
-)
-test.exe
-popd
+call build
+build\mass.exe
