8b2050646a7bd45cb167810d28ac46519aff1a2c
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2020-06-27 14:52:32 +0200

    Add optional and section headers

--- pe32.c
+++ pe32.c
@@ -1,6 +1,7 @@
 #include "prelude.h"
 #include "assert.h"
 
+// ALso contains "Rich" header
 const s8 DOS_PROGRAM_BYTES[] = {
   0x0E, 0x1F, 0xBA, 0x0E, 0x00, 0xB4, 0x09, 0xCD, 0x21, 0xB8, 0x01, 0x4C,
   0xCD, 0x21, 0x54, 0x68, 0x69, 0x73, 0x20, 0x70, 0x72, 0x6F, 0x67, 0x72,
@@ -47,16 +48,111 @@ void write_executable() {
     .Characteristics = IMAGE_FILE_EXECUTABLE_IMAGE | IMAGE_FILE_LARGE_ADDRESS_AWARE,
   };
 
+  IMAGE_OPTIONAL_HEADER64 *optional_header = buffer_allocate(&exe_buffer, IMAGE_OPTIONAL_HEADER64);
+
+  *optional_header = (IMAGE_OPTIONAL_HEADER64) {
+    .Magic = IMAGE_NT_OPTIONAL_HDR64_MAGIC,
+    .MajorLinkerVersion = 0x0E, // FIXME remove or replace once initial implementation is done
+    .MinorLinkerVersion = 0x1A, // FIXME remove or replace once initial implementation is done
+    .SizeOfCode = 0x200, // FIXME calculate based on the amount of machine code
+    .SizeOfInitializedData = 0x400, // FIXME calculate based on the amount of global data
+    .SizeOfUninitializedData = 0, // FIXME figure out difference between initialed and uninitialized
+    .AddressOfEntryPoint = 0x1000, // FIXME resolve to the entry point in the machine code
+    .BaseOfCode = 0x1000, // FIXME resolve to the right section containing code
+    .ImageBase = 0x0000000140000000, // TODO figure out if we should change this
+    .SectionAlignment = 0x1000,
+    .FileAlignment = 0x200,
+    .MajorOperatingSystemVersion = 6, // FIXME figure out if can be not hard coded
+    .MinorOperatingSystemVersion = 0,
+    .MajorSubsystemVersion = 6, // FIXME figure out if can be not hard coded
+    .MinorSubsystemVersion = 0,
+    .SizeOfImage = 0x4000, // FIXME calculate based on the sizes of the sections
+    .SizeOfHeaders = 0x400, // FIXME calculate correctly (as described in MSDN)
+    .Subsystem = IMAGE_SUBSYSTEM_WINDOWS_CUI, // TODO allow user to specify this
+    .DllCharacteristics =
+      IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA |
+      IMAGE_DLLCHARACTERISTICS_NX_COMPAT | // TODO figure out what NX is
+      IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE |
+      IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE,
+    .SizeOfStackReserve = 0x100000,
+    .SizeOfStackCommit = 0x1000,
+    .SizeOfHeapReserve = 0x100000,
+    .SizeOfHeapCommit = 0x1000,
+    .NumberOfRvaAndSizes = IMAGE_NUMBEROF_DIRECTORY_ENTRIES, // TODO think about shrinking this if possible
+    .DataDirectory = {
+      {0}, // Export
+      {.VirtualAddress = 0x20F8, .Size = 0x28}, // Import FIXME calculate this address and size
+      {0}, // Resource
+      {.VirtualAddress = 0x3000, .Size = 0x0C}, // Exception FIXME remove exception info??
+
+      {0}, // Security
+      {0}, // Relocation
+      {.VirtualAddress = 0x2010, .Size = 0x1C}, // Debug FIXME will take a while to implement
+      {0}, // Architecture
+
+      {0}, // Global PTR
+      {0}, // TLS
+      {0}, // Load Config
+      {0}, // Bound Import
+
+      {.VirtualAddress = 0x2000, .Size = 0x10}, // IAT (Import Address Table)  FIXME calculate this address and size
+      {0}, // Delay Import
+      {0}, // CLR
+      {0}, // Reserved
+    },
+  };
+
+  u32 section_offset = optional_header->SizeOfHeaders;
+
+  // .text section
+  IMAGE_SECTION_HEADER *text_section_header = buffer_allocate(&exe_buffer, IMAGE_SECTION_HEADER);
+  *text_section_header = (IMAGE_SECTION_HEADER) {
+    .Name = ".text",
+    .Misc = 0x10, // FIXME size of machine code in bytes
+    .VirtualAddress = optional_header->BaseOfCode,
+    .SizeOfRawData = optional_header->SizeOfCode,
+    .PointerToRawData = section_offset,
+    .Characteristics = IMAGE_SCN_CNT_CODE | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_EXECUTE,
+  };
+  section_offset += text_section_header->SizeOfRawData;
+
+  // .rdata section
+  IMAGE_SECTION_HEADER *rdata_section_header = buffer_allocate(&exe_buffer, IMAGE_SECTION_HEADER);
+  *rdata_section_header = (IMAGE_SECTION_HEADER) {
+    .Name = ".rdata",
+    .Misc = 0x14C, // FIXME size of machine code in bytes
+    .VirtualAddress = 0x2000, // FIXME calculate this
+    .SizeOfRawData = 0x200, // FIXME calculate this
+    .PointerToRawData = section_offset,
+    .Characteristics = IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_MEM_READ,
+  };
+  section_offset += rdata_section_header->SizeOfRawData;
+
+  // .pdata section
+  IMAGE_SECTION_HEADER *pdata_section_header = buffer_allocate(&exe_buffer, IMAGE_SECTION_HEADER);
+  *pdata_section_header = (IMAGE_SECTION_HEADER) {
+    .Name = ".pdata",
+    .Misc = 0x0C, // FIXME size of global data in bytes
+    .VirtualAddress = 0x3000, // FIXME calculate this
+    .SizeOfRawData = 0x200, // FIXME calculate this
+    .PointerToRawData = section_offset,
+    .Characteristics = IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_MEM_READ,
+  };
+  section_offset += pdata_section_header->SizeOfRawData;
+
+  // NULL header telling that the list is done
+  *buffer_allocate(&exe_buffer, IMAGE_SECTION_HEADER) = (IMAGE_SECTION_HEADER){0};
+
   /////////
 
   HANDLE file = CreateFile(
-     L"build\\test.exe",       // name of the write
-     GENERIC_WRITE,          // open for writing
-     0,                      // do not share
-     NULL,                   // default security
-     CREATE_ALWAYS,          // create new file only
-     FILE_ATTRIBUTE_NORMAL,  // normal file
-    NULL                    // no attr. template
+    L"build\\test.exe",    // name of the write
+    GENERIC_WRITE,         // open for writing
+    0,                     // do not share
+    0,                     // default security
+    CREATE_ALWAYS,         // create new file only
+    FILE_ATTRIBUTE_NORMAL, // normal file
+    0                      // no attr. template
   );
 
   assert(file != INVALID_HANDLE_VALUE);
