aabfc94ba01753cb561e54ec87528d3259f6da28
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2020-12-21 20:20:02 +0100

    Support setting explicit compiler in Linux build

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
+# You can explicitly set the C compiler by providing a CC environment variable.
+# This also works inline: CC=clang ./build.sh
+CC="${CC:-cc}"
+
 FLAGS="-std=c11 -g -O0 -pthread"
 
-if [[ $(cc -v 2>&1) == *"clang version"* ]]
+if [[ $($CC -v 2>&1) == *"clang version"* ]]
 then
   FLAGS="-std=c11 -g -O0 -pthread -Wno-tautological-constant-out-of-range-compare -Wno-initializer-overrides"
 fi
@@ -11,15 +15,15 @@ fi
 rm -rf build
 mkdir -p build
 
-cc $FLAGS generate_types.c -o build/generate_types -lm
+$CC $FLAGS generate_types.c -o build/generate_types -lm
 
 cd build
 ./generate_types
 cd ..
 
-cc $FLAGS mass.c -o build/mass -lm
+$CC $FLAGS mass.c -o build/mass -lm
 
-cc $FLAGS function_spec.c -o build/function_spec -lm
+$CC $FLAGS function_spec.c -o build/function_spec -lm
 
 # We can compile but not yet run source_spec so adding a suffix _norun to the binary
-cc $FLAGS source_spec.c -o build/source_spec_norun -lm
+$CC $FLAGS source_spec.c -o build/source_spec_norun -lm
