2026ff455d92f1882aced0a410f9aa004c2ad3c5
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2021-01-31 06:15:49 +0100

    Add a batch file to measure lines of code

---
+++ loc.bat
@@ -0,0 +1,12 @@
+scc .\source.c .\source.h ^
+    .\source_spec.c ^
+    .\value.c .\value.h ^
+    .\function.c .\function.h ^
+    .\function_spec.c ^
+    .\encoding.c .\encoding.h ^
+    .\win32_platform.h ^
+    .\win32_runtime.h ^
+    .\instruction.c ^
+    .\generate_types.c ^
+    .\pe32.c ^
+    .\mass.c
