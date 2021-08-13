95cc701fd05150c9fa471fd68c65e286d52dece7
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2021-08-13 22:34:04 +0200

    Export `address_of` in the compiler scope instead of a global one

--- generated_exports.c
+++ generated_exports.c
@@ -166,6 +166,12 @@ compiler_scope_define_exports(
   );
   MASS_DEFINE_FUNCTION(
     Descriptor_Function_Flags_None | Descriptor_Function_Flags_Compile_Time | Descriptor_Function_Flags_Intrinsic,
+    mass_address_of, "address_of", &descriptor_value_pointer,
+    MASS_FN_ARG("context", &descriptor_execution_context_pointer),
+    MASS_FN_ARG("args", &descriptor_value_view)
+  );
+  MASS_DEFINE_FUNCTION(
+    Descriptor_Function_Flags_None | Descriptor_Function_Flags_Compile_Time | Descriptor_Function_Flags_Intrinsic,
     mass_allocate, "allocate", &descriptor_value_pointer,
     MASS_FN_ARG("context", &descriptor_execution_context_pointer),
     MASS_FN_ARG("args", &descriptor_value_view)
@@ -281,12 +287,6 @@ global_scope_define_exports(
   scope_define_value(
     scope, VALUE_STATIC_EPOCH, COMPILER_SOURCE_RANGE,
     slice_literal("Number_Literal"), type_number_literal_value
-  );
-  MASS_DEFINE_FUNCTION(
-    Descriptor_Function_Flags_None | Descriptor_Function_Flags_Compile_Time | Descriptor_Function_Flags_Intrinsic,
-    mass_address_of, "address_of", &descriptor_value_pointer,
-    MASS_FN_ARG("context", &descriptor_execution_context_pointer),
-    MASS_FN_ARG("args", &descriptor_value_view)
   );
   MASS_DEFINE_FUNCTION(
     Descriptor_Function_Flags_None | Descriptor_Function_Flags_Compile_Time | Descriptor_Function_Flags_Intrinsic,
