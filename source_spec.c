c84b336cc72ce2977c41b905f840c5211c3054db
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2021-11-08 13:41:56 +0100

    Temporarily remove no-else `if` statement support

--- source_spec.c
+++ source_spec.c
@@ -513,11 +513,12 @@ spec("source") {
       );
       check(test_context.result->tag == Mass_Result_Tag_Error);
     }
-    it("should be able to parse and run if statement") {
+    // FIXME
+    xit("should be able to parse and run if statement") {
       s8(*checker)(s32) = (s8(*)(s32))test_program_inline_source_function(
         "is_positive", &test_context,
         "is_positive :: fn(x : s32) -> (s8) {\n"
-          "if (x < 10) { return 0 }\n"
+          "if x < 10 { return 0 }\n"
           "1\n"
         "}"
       );
@@ -768,7 +769,7 @@ spec("source") {
     it("should be able to have an explicit return") {
       s32(*checker)(s32) = (s32(*)(s32))test_program_inline_source_function(
         "checker", &test_context,
-        "checker :: fn(x : s32) -> (s32) { if x > 0 { return x }; 0 }"
+        "checker :: fn(x : s32) -> (s32) { if x > 0 { return x } else {}; 0 }"
       );
       check(spec_check_mass_result(test_context.result));
       s32 actual = checker(42);
@@ -779,8 +780,7 @@ spec("source") {
       s64(*fibonnacci)(s64) = (s64(*)(s64))test_program_inline_source_function(
         "fibonnacci", &test_context,
         "fibonnacci :: fn(n : s64) -> (s64) {\n"
-          "if (n < 2) { return n }\n"
-          "fibonnacci(n - 1) + fibonnacci(n - 2)"
+          "if n < 2 then n else fibonnacci(n - 1) + fibonnacci(n - 2)"
         "}"
       );
       check(spec_check_mass_result(test_context.result));
@@ -1429,7 +1429,7 @@ spec("source") {
       test_program_inline_source_base(
         "DUMMY", &test_context,
         "do_stuff :: fn() -> () {}\n"
-        "if 1 { do_stuff() }\n"
+        "if 1 { do_stuff() } else {}\n"
         "DUMMY :: 42"
       );
       check(test_context.result->tag == Mass_Result_Tag_Success);
@@ -1509,7 +1509,7 @@ spec("source") {
     it("should be able to parse and run macro id fn with an explicit return and an immediate arg") {
       s64(*checker)(void) = (s64(*)(void))test_program_inline_source_function(
         "checker", &test_context,
-        "id :: macro(x : s64) -> (s64) { if (x > 0) { return 20 }; x }\n"
+        "id :: macro(x : s64) -> (s64) { if (x > 0) { return 20 } else {}; x }\n"
         "checker :: fn() -> (s64) { id(42) + 1 }"
       );
       check(spec_check_mass_result(test_context.result));
@@ -1694,7 +1694,7 @@ spec("source") {
           "sum : s32;"
           "sum = 0;"
           "label loop;"
-          "if (x < 0) { return sum };"
+          "if (x < 0) { return sum } else {};"
           "sum = sum + x;"
           "x = x - 1;"
           "goto loop;"
