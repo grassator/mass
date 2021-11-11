ed7638b2ce699c071f2857550a1fac641173e6b8
Dmitriy Kubyshkin <dmitriy@kubyshkin.name>
2021-11-11 04:30:36 +0100

    Update loc.bat with the right files

--- loc.bat
+++ loc.bat
@@ -1,12 +1,16 @@
-scc .\source.c .\source.h ^
-    .\source_spec.c ^
-    .\value.c .\value.h ^
-    .\function.c .\function.h ^
-    .\function_spec.c ^
-    .\encoding.c .\encoding.h ^
-    .\win32_platform.h ^
-    .\win32_runtime.h ^
-    .\instruction.c ^
-    .\generate_types.c ^
-    .\pe32.c ^
-    .\mass.c
+scc ^
+  calling_convention.h ^
+  encoding.c ^
+  function.h function.c ^
+  instruction.c ^
+  mass.c ^
+  meta.c ^
+  pe32.c ^
+  posix_runtime.h ^
+  program.h program.c ^
+  source.h source.c ^
+  tokenizer.re.c ^
+  types.h ^
+  value.h value.c ^
+  win32_platform.h ^
+  win32_runtime.h
